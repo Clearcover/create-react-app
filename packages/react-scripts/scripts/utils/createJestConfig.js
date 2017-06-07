fa0de303ebe83712b8a13f27d15d65b69267dc59
Sunny Vempati <sunnyvempati@gmail.com>
2017-06-07 17:02:40 -0500

    added route to filemock

--- packages/react-scripts/scripts/utils/createJestConfig.js
+++ packages/react-scripts/scripts/utils/createJestConfig.js
@@ -43,6 +43,7 @@ module.exports = (resolve, rootDir, isEjecting) => {
     moduleNameMapper: {
       '^react-native$': 'react-native-web',
       '^.+\\.css$': 'identity-obj-proxy',
+      "^.+\\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/test-utils/fileMock.js"
     },
   };
   if (rootDir) {
